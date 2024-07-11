cfd049b70c5f97a2a243395907385b2f1f7baa36
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-07-11 22:16:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,3 +63,8 @@ LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_I
 LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+
+VR_COM_photo = "https://telegra.ph/file/a00c405a374d21ea7cfb7.jpg"
+VR_photo = "https://telegra.ph/file/a35a995c9c411048adfab.jpg"
+VR_LOG = "-1001855482644"
+
