1a530015c58ba2e2f19b6898a787cf2f6da83813
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-07-11 22:36:46 +0530

    Create verify_db.py

---
+++ database/verify_db.py
@@ -0,0 +1,36 @@
+from datetime import timedelta, datetime
+from pymongo import MongoClient
+import os
+from info import DATABASE_URL, DATABASE_NAME
+from os import environ
+import pytz
+
+client = MongoClient(DATABASE_URL)
+datab = client[f"{DATABASE_NAME}"]
+db = datab.verifications
+kolkata_timezone = pytz.timezone('Asia/Kolkata')
+
+def save_verification(user_id):
+    now = datetime.now(pytz.timezone('Asia/Kolkata'))
+    verification = {"user_id": user_id, "verified_at": now}
+    collection = db["verifications"]
+    collection.insert_one(verification)
+
+def get_verifications_count(time_period):
+    collection = db[f"verifications"]
+    if time_period == 'today':
+        start_datetime = datetime.now(kolkata_timezone).replace(hour=0, minute=0, second=0, microsecond=0)
+        end_datetime = datetime.now(kolkata_timezone)
+    elif time_period == 'yesterday':
+        start_datetime = datetime.now(kolkata_timezone).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+        end_datetime = start_datetime + timedelta(days=1)
+    elif time_period == 'this_week':
+        start_datetime = datetime.now(kolkata_timezone).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=datetime.now(kolkata_timezone).weekday())
+        end_datetime = datetime.now(kolkata_timezone)
+    elif time_period == 'this_month':
+        start_datetime = datetime.now(kolkata_timezone).replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        end_datetime = datetime.now(kolkata_timezone)
+    else:
+        raise ValueError("Invalid time period")
+    count = collection.count_documents({'verified_at': {'$gt': start_datetime, '$lt': end_datetime}})
+    return count
