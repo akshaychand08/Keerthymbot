e2f2383fdd482c8d636db846bfb1bfd2fcd8fb6a
Yashwantcine <cinemicabot2024@gmail.com>
2024-07-15 21:52:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '5489251921:AAErspZvDjpYarN391qs48oY2brj_JS
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/68feb51ce9c55c31d3265.png')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1128389435 5277484262 1353788579 1163343128 905710386').split()]
@@ -85,7 +85,7 @@ SHORT_MODE = is_enabled((environ.get('SHORT_MODE', "True")), True)
 update_channel = "-1002236763731"
 
 BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "-1002228911934")) 
-GEN_URL = os.environ.get("GEN_URL", "https://substantial-eryn-cinemicastreamer-a599f26c.koyeb.app/") # https://example.com/
+GEN_URL = os.environ.get("GEN_URL", "https://troubled-lianne-ettest-bcdcdb8d.koyeb.app/") # https://example.com/
 
 
 
