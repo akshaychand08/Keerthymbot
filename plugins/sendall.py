16add06cdf3cf9305e5fc949d4323de7adbb9f61
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-08-06 22:13:25 +0530

    Update sendall.py

--- plugins/sendall.py
+++ plugins/sendall.py
@@ -26,7 +26,7 @@ async def send_all_files(client, message, files, chat_id, grp_id):
             [
                 [
                     InlineKeyboardButton(
-                        "⚡️ 𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 / 𝙒𝙖𝙩𝙘𝙝 𝙊𝙣𝙡𝙞𝙣𝙚 ⚡️", callback_data=f"stream_link:{files.file_id}:{message.from_user.id}"
+                        "⚡️ 𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 / 𝙒𝙖𝙩𝙘𝙝 𝙊𝙣𝙡𝙞𝙣𝙚 ⚡️", callback_data=f"stream_link:{file.file_id}:{message.from_user.id}"
                     )
                 ],
                 [            
