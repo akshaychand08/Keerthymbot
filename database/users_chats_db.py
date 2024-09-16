49d582fcf46d80f060860141dd5450c5fd198c5c
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2024-09-16 08:33:36 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -216,6 +216,7 @@ class Database:
             res = {
                 "user_id": user_id,
                 "last_verified": datetime.datetime(2020, 5, 17, 0, 0, 0, tzinfo=ist_timezone),
+                
             }
             user = await self.misc.insert_one(res)
             user = await self.misc.find_one({"user_id": user_id})
