776119be7d0ab1106a929571912afe7f6697b158
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-04-16 08:21:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -350,7 +350,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
     btn = []
     for file in files:        
         btn.append([
-            InlineKeyboardButton(text=f"⚡️ {get_size(file.file_size)}» {remove_username(file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=files_{grp_id}_{file.file_id}')
+            InlineKeyboardButton(text=f"⚡️ {get_size(file.file_size)}» {remove_username(file.file_name)}", callback_data=f'files#{grp_id}_{file.file_id}')
         ])    
     if l_offset != "":
         btn.append(
